240228b968d67de87f1b3fd0d6ca82683c9d2abc
Thomas Kemmer <tkemmer@computer.org>
2015-06-17 17:21:39 +0200

    Cleanup test environment.

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,22 +1,19 @@
 from __future__ import unicode_literals
 
-import unittest
-
 from mopidy_local_images import Extension
 
 
-class ExtensionTest(unittest.TestCase):
+def test_get_default_config():
+    ext = Extension()
+    config = ext.get_default_config()
+    assert '[local-images]' in config
+    assert 'enabled = true' in config
 
-    def test_get_default_config(self):
-        ext = Extension()
-        config = ext.get_default_config()
-        self.assertIn('[local-images]', config)
-        self.assertIn('enabled = true', config)
 
-    def test_get_config_schema(self):
-        ext = Extension()
-        schema = ext.get_config_schema()
-        self.assertIn('library', schema)
-        self.assertIn('base_uri', schema)
-        self.assertIn('image_dir', schema)
-        self.assertIn('album_art_files', schema)
+def test_get_config_schema():
+    ext = Extension()
+    schema = ext.get_config_schema()
+    assert 'library' in schema
+    assert 'base_uri' in schema
+    assert 'image_dir' in schema
+    assert 'album_art_files' in schema
